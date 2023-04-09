57aa390581ee64fe9292c60140a86fe0bfd635e5
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-04-09 15:39:24 +0600

    added security configuration for OAuth2 resource server

---
+++ src/main/java/com/pract/jwtbasedoauth2/security/WebSecurity.java
@@ -0,0 +1,38 @@
+package com.pract.jwtbasedoauth2.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Slf4j
+public class WebSecurity {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .antMatchers("api/auth/*").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf().disable()
+                .cors().disable()
+                .httpBasic().disable()
+                .oauth2ResourceServer((ouath2) ->
+                        ouath2.jwt((jwt) -> jwt.jwtAuthenticationConverter(/*to do*/))
+                )
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling((exceptions) -> exceptions
+                        .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
+                );
+        return http.build();
+    }
+}
