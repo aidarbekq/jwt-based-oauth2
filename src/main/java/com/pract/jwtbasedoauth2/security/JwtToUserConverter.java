d017c80f88ae0498225e9989b8955c738b6a25ef
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-04-09 16:02:28 +0600

    add JwtToUserConverter class for JWT to User conversion

---
+++ src/main/java/com/pract/jwtbasedoauth2/security/JwtToUserConverter.java
@@ -0,0 +1,21 @@
+package com.pract.jwtbasedoauth2.security;
+
+import com.pract.jwtbasedoauth2.document.User;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+public class JwtToUserConverter implements Converter<Jwt, UsernamePasswordAuthenticationToken> {
+
+
+    @Override
+    public UsernamePasswordAuthenticationToken convert(Jwt jwt) {
+        User user = new User();
+        user.setId(jwt.getSubject());
+        return new UsernamePasswordAuthenticationToken(user, jwt, Collections.EMPTY_LIST);
+    }
+}
