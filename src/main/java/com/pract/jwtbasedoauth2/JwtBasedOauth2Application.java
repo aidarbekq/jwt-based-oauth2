2b6aee2e6da1073210c0d6ad6f7c6225f50edf65
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-04-09 15:07:17 +0600

    add project

---
+++ src/main/java/com/pract/jwtbasedoauth2/JwtBasedOauth2Application.java
@@ -0,0 +1,13 @@
+package com.pract.jwtbasedoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtBasedOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtBasedOauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/pract/jwtbasedoauth2/JwtBasedOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pract.jwtbasedoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtBasedOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
