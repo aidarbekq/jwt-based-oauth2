55a04fa5b38261e7ed1bdc6871e9fccf95ca8686
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-04-11 13:05:53 +0600

    create dto for signup

---
+++ src/main/java/com/pract/jwtbasedoauth2/dto/SignupDto.java
@@ -0,0 +1,11 @@
+package com.pract.jwtbasedoauth2.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignupDto {
+    private String username;
+    private String password;
+}
